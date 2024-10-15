fb56bce5e603fc2a3f2593347eefc617dba93ebe
Abhijith K P <abhijithkp773@gmail.com>
2024-10-15 11:37:52 +0530

    feat: update test endpoint

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
 import ResponseHelper from '../helpers/response';
+import packageInfo from '../../package.json';
+
+const startTime = Date.now();
 
 export default class TestController {
     static testEndpoint(req: Request, res: Response): void {
+        const uptime = Math.floor((Date.now() - startTime) / 1000);
+        const data = {
+            version: packageInfo.version,
+            uptime: `${uptime} seconds`,
+            timeStamp: new Date(Date.now()),
+        };
         return ResponseHelper.success(
             res,
+            data,
             'API is up and running successfully'
         );
     }
